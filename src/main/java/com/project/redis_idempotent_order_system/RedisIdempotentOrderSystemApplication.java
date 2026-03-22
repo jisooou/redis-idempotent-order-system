074ea325c8cb2110b821c6dad4d767c7cd264091
jisooou <jisoou4@gmail.com>
2026-03-22 19:01:59 +0900

    chore: 프로젝트 초기환경 설정

---
+++ src/main/java/com/project/redis_idempotent_order_system/RedisIdempotentOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.redis_idempotent_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisIdempotentOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisIdempotentOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/redis_idempotent_order_system/RedisIdempotentOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.redis_idempotent_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisIdempotentOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
